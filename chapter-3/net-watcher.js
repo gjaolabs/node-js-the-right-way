44348d79c00be3cbe6c1c2e83ac88e91cba4fdc9
nodus8tollens <mladenovskipredrag@gmail.com>
2023-12-04 21:36:19 +0100

    TCP server/subscriber file watch

---
+++ chapter-3/net-watcher.js
@@ -0,0 +1,38 @@
+"use strict"
+
+const fs = require("fs");
+const net = require("net")
+
+const port = 8080;
+const filename = process.argv[2]
+
+//Creates a TCP server that writes to the client/subscriber (at port 8080) when modifications to "target.exe" file take place
+const server = net.createServer((connection) => {
+    console.log("Subscriber connected.")
+    connection.write("Now watching " + filename + " for changes.\n");
+
+    //Listens for events on "target.exe" and notifies the client/subscriber
+    let watcher = fs.watch(filename, () => {
+        connection.write("File " + filename + " changed at: " + Date.now() + "\n")
+
+    })
+
+    //Listens for the "close" event and appropriately closes the watcher process
+    connection.on("close", () => {
+        console.log("Subscriber disconnected.")
+        watcher.close()
+    })
+})
+
+if(!filename){
+    throw Error("No target filename was specified.")
+}
+
+server.listen(port, () => {
+    console.log(`Listening for subscribers on port ${port}...`)
+})
+
+//Start sparate terminal processes
+//One for node via process.argv i.e. node net-watcher.js target.exe
+//One for client/subscriber i.e. telnet localhost 8080
+//One for manipulating the file i.e. touch target.txt
